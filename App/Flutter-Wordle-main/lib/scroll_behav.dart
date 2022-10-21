2953909292a1764bfa4b568df7b9a1a5515efdae
19021362 <19021362@vnu.edu.vn>
2022-10-21 14:56:24 +0700

    Thay đổi đường dẫn mã nguồn

---
+++ App/Flutter-Wordle-main/lib/scroll_behav.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+
+class MyScrollBehavior extends ScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+      };
+}
